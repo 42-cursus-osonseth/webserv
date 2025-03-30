0a77b5ea1a67a4108f8fc451dea30136f2e1c110
Twigister <43752955+Twigister@users.noreply.github.com>
2025-03-30 01:27:18 +0100

    Removed duplicates for merge process

--- includes/checkFiles.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CHECKFILES_HPP
-#define CHECKFILES_HPP
-
-#include "library.hpp"	
-bool						isValidFormat(const std::string &filename);
-bool						isValidFile(const std::string &filename);
-bool						isValidDir(const std::string &path);
-std::string					formatPath(const std::string &path);
-#endif
